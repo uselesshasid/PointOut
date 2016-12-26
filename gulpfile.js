85a2615b864eb067d852606a4071939b2b3c08f4
mbh <mbh@Joels-Mac-mini.home>
2016-12-26 09:50:32 -0500

    gulp zip version

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,10 @@
 ﻿/// <binding AfterBuild='zipfiles' />
 var gulp = require("gulp");
 var zip = require("gulp-zip");
+var version = require('./manifest.json').version;
 
 gulp.task("zipfiles", function () {
     gulp.src(['scripts/*', 'css/*', 'icon.png', 'manifest.json', 'images/*'], { base: './' })
-        //TODO: read config file
-        //TODO: extract the version number
-        //TODO: suffix file name with the version number i.e. PointOut_v1.1.0.zip
-        .pipe(zip('PointOut.zip'))
+        .pipe(zip('PointOut_v' + version + '.zip'))
         .pipe(gulp.dest('dist'));
 })
