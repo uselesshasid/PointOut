518967fa23c547f3d8bae7d005591f8a47ad81de
UselessHasid <uselesshasid@gmail.com>
2017-01-18 19:15:51 -0500

    Refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,33 @@
 var gulp = require("gulp");
 var zip = require("gulp-zip");
 var ts = require("gulp-typescript");
+var gulpclean = require("gulp-clean");
 
 var version = require('./manifest.json').version;
 
 var tsProject = ts.createProject("tsconfig.json", {
 })
 
-gulp.task("compile-ts", function(){
-    gulp.src('scripts/**/*.ts')
+gulp.task("build", function(){
+    gulp.src('src/**/*.ts')
     .pipe(tsProject())
     .pipe(gulp.dest('scripts'));
+})
+
+
+gulp.task("copyfiles",function(){
+    gulp.src('scripts\*',{read:false}).pipe(gulpclean());
+    gulp.src([
+        "node_modules/jquery/dist/jquery.min.js", 
+        "node_modules/jquery-ui/dist/jquery-ui.js", 
+        "node_modules/jquery-scrollto/out/lib/jquery-scrollto.js", 
+        "node_modules/toastr/build/toastr.min.js",
+        "src/background.js",
+        "src/contentscript.js",
+        "src/dom.js",
+        "src/jquery.extensions.js"
+        ])
+        .pipe(gulp.dest('scripts'));
 })
 
 gulp.task("zipfiles", function () {
@@ -21,5 +38,5 @@ gulp.task("zipfiles", function () {
 })
 
 gulp.task('watch',function(){
-    gulp.watch('scripts/**/*.ts',['compile-ts']);
+    gulp.watch('src/**/*.ts',['build']);
 })
--- src/jquery.extensions.js
+++ src/jquery.extensions.js
@@ -1,4 +1,4 @@
-﻿jQuery.fn.center = function (target) {
+jQuery.fn.center = function (target) {
     var loc = target.offset();
     this.css("top", (target.outerHeight() - this.outerHeight()) / 2 + loc.top + 'px');
     this.css("left", (target.outerWidth() - this.outerWidth()) / 2 + loc.left + 'px');
